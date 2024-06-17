ce68773951d0e4f79ee5be5d1f4edec2490a6a0a
Dimitris Papathanasiou <dimitrispapathanasioy00@gmail.com>
2024-06-17 12:16:55 +0300

    fixed filters images

--- Figures Generation/figure_5_generation.py
+++ Figures Generation/figure_5_generation.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ast
 
-
 def extract_overlaps(file_name, number_of_nodes):
     data = pd.read_csv(file_name, header=None, names=[
         'model', 'random', 'worst_overlap', 'time', 'nodes', 'filters'])
@@ -17,14 +16,12 @@ def extract_overlaps(file_name, number_of_nodes):
 
     averages = {}
 
-    for num_filters in [10, 30, 50]:
+    for num_filters in [10, 30]:  # Exclude 50
         data_filters = data_filtered[data_filtered['filters'] == num_filters]
 
         best_case = np.mean([lst[0] for lst in data_filters['model']])
-        our_selection_avg = np.mean([np.mean(lst)
-                                    for lst in data_filters['model']])
-        random_model_avg = np.mean([np.mean(lst)
-                                   for lst in data_filters['random']])
+        our_selection_avg = np.mean([np.mean(lst) for lst in data_filters['model']])
+        random_model_avg = np.mean([np.mean(lst) for lst in data_filters['random']])
         worst_case_avg = np.mean(data_filters['worst_overlap'])
 
         averages[num_filters] = {
@@ -36,62 +33,32 @@ def extract_overlaps(file_name, number_of_nodes):
 
     return averages
 
-
-# Extract overlaps for 100 nodes
-overlaps_100 = extract_overlaps('results_4.csv', 100)
-
 # Extract overlaps for 200 nodes
 overlaps_200 = extract_overlaps('results_4.csv', 200)
 
-# Plotting for 100 nodes
-plt.figure(figsize=(10, 5))
-colors = ['b', 'g', 'r']
+# Plotting for 200 nodes
+plt.figure(figsize=(16, 6))  # Increase figure size horizontally
+colors = ['g', 'b', 'm', 'r']
 markers = ['o', 'x', '^', 's']
 linestyles = ['-', '--', '-.', ':']
 
-for i, (num_filters, overlap_dict) in enumerate(overlaps_100.items()):
-    plt.plot([10, 30, 50], [overlap_dict['best_case']] * 3, linestyle=linestyles[i], marker=markers[i],
-             color=colors[i], label=f'Best case: {num_filters}')
-    plt.plot([10, 30, 50], [overlap_dict['our_selection']] * 3, linestyle=linestyles[i], marker=markers[i],
-             color=colors[i], label=f'Our Selection: {num_filters}')
-    plt.plot([10, 30, 50], [overlap_dict['random_model']] * 3, linestyle=linestyles[i], marker=markers[i],
-             color=colors[i], label=f'Random Model: {num_filters}')
-    plt.plot([10, 30, 50], [overlap_dict['worst_case']] * 3, linestyle=linestyles[i], marker=markers[i],
-             color=colors[i], label=f'Worst Case: {num_filters}')
-
-plt.yticks(np.arange(4, 5.6, step=0.1))
-plt.xticks([10, 30, 50])
-plt.xlabel('Number of Filters')
-plt.ylabel('Average Overlap')
-plt.grid(True)
-plt.tight_layout(rect=[0, 0, 0.75, 1])  # Adjust the layout
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig('Figures/plot_100_nodes_diff_num_filters.eps',
-            format='eps', bbox_inches="tight")
-plt.show()
-plt.close()
-
-
-# Plotting for 200 nodes
-plt.figure(figsize=(10, 5))
 for i, (num_filters, overlap_dict) in enumerate(overlaps_200.items()):
-    plt.plot([10, 30, 50], [overlap_dict['best_case']] * 3, linestyle=linestyles[i], marker=markers[i],
-             color=colors[i], label=f'Best case: {num_filters}')
-    plt.plot([10, 30, 50], [overlap_dict['our_selection']] * 3, linestyle=linestyles[i], marker=markers[i],
-             color=colors[i], label=f'Our Selection: {num_filters}')
-    plt.plot([10, 30, 50], [overlap_dict['random_model']] * 3, linestyle=linestyles[i], marker=markers[i],
-             color=colors[i], label=f'Random Model: {num_filters}')
-    plt.plot([10, 30, 50], [overlap_dict['worst_case']] * 3, linestyle=linestyles[i], marker=markers[i],
-             color=colors[i], label=f'Worst Case: {num_filters}')
+    plt.plot([10, 30], [overlap_dict['best_case']] * 2, linestyle=linestyles[i], marker=markers[i],
+             color=colors[0], label=f'Best Case: {num_filters} Dimensions')
+    plt.plot([10, 30], [overlap_dict['our_selection']] * 2, linestyle=linestyles[i], marker=markers[i],
+             color=colors[1], label=f'Proposed method: {num_filters} Dimensions')
+    plt.plot([10, 30], [overlap_dict['random_model']] * 2, linestyle=linestyles[i], marker=markers[i],
+             color=colors[2], label=f'Random Migration Model: {num_filters} Dimensions')
+    plt.plot([10, 30], [overlap_dict['worst_case']] * 2, linestyle=linestyles[i], marker=markers[i],
+             color=colors[3], label=f'Worst Case: {num_filters} Dimensions')
 
-plt.yticks(np.arange(4, 5.6, step=0.1))
-plt.xticks([10, 30, 50])
-plt.xlabel('Number of Filters')
-plt.ylabel('Average Overlap')
+plt.yticks(np.arange(4, 5.6, step=0.1), fontsize=12)
+plt.xticks([10, 30], fontsize=12)  # Set font size for x-axis ticks
+plt.xlabel('Number of Filters', fontsize=14)  # Increase font size for x-axis label
+plt.ylabel('Overlap', fontsize=14)  # Increase font size for y-axis label
 plt.grid(True)
 plt.tight_layout(rect=[0, 0, 0.75, 1])  # Adjust the layout
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig('Figures/plot_200_nodes_diff_num_filters.eps',
-            format='eps', bbox_inches="tight")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop={'size': 12})
+plt.savefig('Figures/plot_200_nodes_diff_num_filters.eps', format='eps', bbox_inches="tight")
 plt.show()
 plt.close()
